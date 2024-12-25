65a1d15aa19a7e80fe2902ae64b8091b4c1c0a24
Salma-Yahya24 <salmayahya1812@gmail.com>
2024-12-25 10:17:25 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+
+    document.addEventListener("DOMContentLoaded", function() {
+        var typed = new Typed('#typed-output', {
+            strings: ["ASP.Net Developer", "Salma Yahya","Angaular Developer"],
+            typeSpeed: 70,    // سرعة الكتابة (بالمللي ثانية)
+            backSpeed: 50,    // سرعة المسح (بالمللي ثانية)
+            loop: true        // إذا كنت تريد التكرار
+        });
+    });
+
